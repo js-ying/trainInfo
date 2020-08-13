6a685f047c57fa5bddca8ff2fc53d37e5cc63f62
Kevin Ying <kevin.js.ying@ibm.com>
2020-08-13 22:28:10 +0800

    微調「關於」超連結

--- src/components/about.vue
+++ src/components/about.vue
@@ -61,12 +61,12 @@
           </b-col>
           <b-col cols="12" class="mt-5">
             <!-- 返回按鈕 -->
-            <button type="button" class="btn btn-secondary btn-sm mt-3 mb-4" @click="goHome()">
+            <button type="button" class="btn btn-secondary btn-sm mt-3 mb-4" @click="goBack()">
               <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-arrow-return-left" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M5.854 5.646a.5.5 0 0 1 0 .708L3.207 9l2.647 2.646a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 0 1 .708 0z"/>
                 <path fill-rule="evenodd" d="M13.5 2.5a.5.5 0 0 1 .5.5v4a2.5 2.5 0 0 1-2.5 2.5H3a.5.5 0 0 1 0-1h8.5A1.5 1.5 0 0 0 13 7V3a.5.5 0 0 1 .5-.5z"/>
               </svg>
-              回首頁
+              返回
             </button>
           </b-col>
         </b-row>
@@ -112,6 +112,9 @@ export default {
   methods: {
     goHome() {
       this.$router.push({ name: 'Home' }).catch(() => { });
+    },
+    goBack() {
+      window.history.back();
     }
   }
 };
