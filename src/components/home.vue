258a504ffe1e381c1f41f37564524da6b59f889f
JhihSi Ying <jhihsiying@gmail.com>
2023-08-25 01:42:57 +0800

    [update] 增加免責聲名於首頁

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="home">
     <search-area></search-area>
+
+    <!-- 免責聲明 -->
+    <div id="disclaimer">
+      本站所提供之資訊僅供參考，請依照現場鐵路狀況或臺鐵官方相關管理單位所公佈之資訊為準。
+    </div>
   </div>
 </template>
 
@@ -24,4 +29,14 @@ export default {
 </script>
 
 <style scoped>
+#disclaimer {
+  font-size: 0.9rem;
+  padding: 0 1rem 1.2rem 1rem;
+}
+
+@media screen and (max-width: 768px) {
+  #disclaimer {
+    font-size: 0.8rem;
+  }
+}
 </style>
